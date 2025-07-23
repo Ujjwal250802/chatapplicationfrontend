c19e8ef3acbbbb3a5d7ecc8b61bef22bde09b3ee
Ujjwal Kaushik <166251292+Ujjwal250802@users.noreply.github.com>
2025-07-23 13:06:33 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL ="https://chatapplicationbackend-2.onrender.com";
+const BASE_URL ="https://chatapplicationbackend-2.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
